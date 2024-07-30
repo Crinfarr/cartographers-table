28bab59ec5efe97d450a5b00ad91d9373271dc7c
Crinfarr <dev@crinfarr.io>
2024-07-30 01:32:12 -0500

    put clothes on the window

--- src/Main.hx
+++ src/Main.hx
@@ -4,9 +4,10 @@ import haxe.ui.HaxeUIApp;
 import views.MainView;
 
 class Main {
-	public static final app:HaxeUIApp = new HaxeUIApp();
+	public static var app:HaxeUIApp;
 
     static function main() {
+		app = new HaxeUIApp();
         app.ready(() -> {
             app.title = 'Cartographer\'s Table';
             app.addComponent(new MainView());
