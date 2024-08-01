26859ded8bb61b4e6f5ffc02714eba5395d1bc94
Asher Hoeveler <dev@crinfarr.io>
2024-08-01 16:32:34 -0500

    abstract instead of class

--- src/modules/Item.hx
+++ src/modules/Item.hx
@@ -1,39 +1,35 @@
 package modules;
 
+import haxe.io.Bytes;
 import haxe.Exception;
 
-abstract Item(String) from String to String {
+class Item {
 	public var category(get, never):Null<String>;
 	public var modid(get, never):String;
 	public var name(get, never):String;
-    public var literal(get, never):String;
+    public var literal:String;
+    public var sprite:Bytes;
 
 	public function new(str:String) {
-		this = str;
+		literal = str;
 	}
 
 	private function get_category():Null<String> {
 		final re = ~/(?<=:).+?(?=\/)/gm;
-		if (!re.match(this))
+		if (!re.match(literal))
 			return null;
 		return re.matched(0);
 	}
-
 	private function get_modid():String {
 		final re = ~/^.+?(?=:)/gm;
-		if (!re.match(this))
-			throw new Exception('Cannot detect MODID of $this');
+		if (!re.match(literal))
+			throw new Exception('Cannot detect MODID of $literal');
 		return re.matched(0);
 	}
-
 	private function get_name():String {
-		final re = new EReg('(?<=${this.indexOf('/') == -1 ? ':' : '/'}).+?', gm);
-		if (!re.match(this))
-			throw new Exception('Cannot detect name of $this');
+		final re = new EReg('(?<=${literal.indexOf('/') == -1 ? ':' : '/'}).+?', "gm");
+		if (!re.match(literal))
+			throw new Exception('Cannot detect name of $literal');
 		return re.matched(0);
 	}
-    @:to(String)
-    private function get_literal():String {
-        return this;
-    }
 }
