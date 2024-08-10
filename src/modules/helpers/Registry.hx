334bfa6f98500c78ac23cfef5d5641a346a4340b
Asher Hoeveler <dev@crinfarr.io>
2024-08-09 19:54:01 -0500

    Relocate + iterators

--- src/modules/helpers/Registry.hx
+++ src/modules/helpers/Registry.hx
@@ -1,5 +1,6 @@
-package modules;
+package modules.helpers;
 
+import haxe.io.Bytes;
 import haxe.Exception;
 
 private enum CallbackType {
@@ -11,6 +12,7 @@ class Registry<T> {
     private var callbacks:{added:Array<T->Void>, removed:Array<T->Void>} = {added:[], removed:[]}
 	private var keys:{added:Array<String>, removed:Array<String>}
     private var reg:Map<String, T>;
+    public var size(default, null):Int;
     public function new() {
         reg = new Map<String, T>();
     }
@@ -22,6 +24,7 @@ class Registry<T> {
     public function assign(key:String, value:T) {
         if (reg.exists(key)) throw new Exception('Cannot reassign ${key}: Current value is ${reg[key]}');
 		reg.set(key, value);
+        size++;
         for (cb in this.callbacks.added) {
             cb(value);
         }
@@ -31,8 +34,9 @@ class Registry<T> {
         for (cb in this.callbacks.removed) {
             cb(reg.get(key));
         }
-        this.reg.remove(key);
-    }
+		this.reg.remove(key);
+        size--;
+	}
     
     public function addCallback(type:CallbackType, callback:T->Void):String {
         final uuid = UUID.v4();
@@ -63,5 +67,14 @@ class Registry<T> {
             default:
                 throw new Exception("Invalid callback type");
         }
+    }
+    public inline function keyIter():Iterator<String> {
+        return this.reg.keys();
+    }
+    public inline function itemIter():Iterator<T> {
+        return this.reg.iterator();
+    }
+    public inline function iterator():KeyValueIterator<String, T> {
+        return this.reg.keyValueIterator();
     }
 }
