cd41db26d8401e8374974bfb4f962d651831d0be
Asher Hoeveler <dev@crinfarr.io>
2024-08-12 18:11:43 -0500

    (later) custom message box

---
+++ src/views/dialogs/NotifyDialog.hx
@@ -0,0 +1,10 @@
+package views.dialogs;
+
+import haxe.ui.containers.dialogs.Dialog;
+
+@:build(haxe.ui.ComponentBuilder.build('src/assets/views/dialogs/NotifyDialog.xml'))
+class NotifyDialog extends Dialog {
+    public function new() {
+        super();
+    }
+}
