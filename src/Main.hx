798d887c75b72becd193cde9d4851ddf558a1db2
Asher Hoeveler <dev@crinfarr.io>
2024-08-12 18:11:31 -0500

    Use single registry for item db

--- src/Main.hx
+++ src/Main.hx
@@ -2,16 +2,14 @@ package ;
 
 import haxe.io.Bytes;
 import haxe.ui.HaxeUIApp;
-import modules.Item;
-import modules.Registry;
+import modules.helpers.Registry;
 import modules.config.ConfigInst;
 import views.MainView;
 
 
 class Main {
 	public static var app:HaxeUIApp;
-    public static var ItemRegistry = new Registry<Item>();
-    public static var TextureRegistry = new Registry<Registry<Bytes>>();
+    public static var ItemRegistry = new Registry<Bytes>();
 	public static var conf:ConfigInst;
     static function main() {
 		app = new HaxeUIApp();
