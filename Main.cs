67360bbf24ace2cca377485c5fc890d4050b0768
Asher Hoeveler <ahoeveler@apotechgroup.com>
2025-01-28 18:56:45 -0600

    Treeview for recipes

--- Main.cs
+++ Main.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Xml.Serialization;
 using Godot;
 
 public partial class Main : Control
@@ -5,8 +7,24 @@ public partial class Main : Control
     InitDialog initPopup = ResourceLoader.Load<PackedScene>("res://assets/popup/InitDialog.tscn").Instantiate<InitDialog>();
     Window progressPopup = ResourceLoader.Load<PackedScene>("res://assets/popup/LoadingDialog.tscn").Instantiate<Window>();
     Config LoadedConfig;
+    Tree sidebar;
+    TreeItem recipeRoot;
+
     public override void _Ready()
     {
+        sidebar = GetNode<Tree>("%Sidebar");
+        recipeRoot = sidebar.CreateItem();
+        recipeRoot.SetText(0, "Recipes");
+        sidebar.ButtonClicked += (clickedItm, _, _, _) =>
+            {
+                clickedItm.SetEditable(0, true);
+            };
+        for (int n = 0; n <= 100; n++)
+        {
+            TreeItem child = recipeRoot.CreateChild();
+            child.AddButton(0, ResourceLoader.Load<Texture2D>("res://assets/icons/pen.svg"));
+            child.SetText(0, n.ToString());
+        }
         initPopup.CloseRequested += () =>
         {
             GetTree().Quit();
